7df98233d0a7107582360cd983f0056ff222d4b0
Tushar patil <tp36213@gmail.com>
2024-06-16 22:50:08 +0530

    some more changes

--- core/settings.py
+++ core/settings.py
@@ -84,6 +84,11 @@ WSGI_APPLICATION = 'core.wsgi.application'
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.dummy',
+    }
+}
 
 
 # Password validation
